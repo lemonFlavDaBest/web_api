e76e05a6f857c2ad9b71bfdd15695fa96d7945f3
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-08 16:47:10 -0700

    Update main.rs
    got it working. slay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    //create a path Filter
+    let hello = warp::path("hello")
+        .map(|| format!("Hello, World!"));
+    
+        //start and the server and pass the route filter to it
+        warp::serve(hello)
+            .run(([127, 0, 0, 1], 3030))
+            .await;
 }
